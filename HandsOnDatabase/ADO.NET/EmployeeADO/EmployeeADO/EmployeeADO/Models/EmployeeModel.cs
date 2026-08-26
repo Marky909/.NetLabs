1c8255069de1101b47ca9258b56c0f2cba5ea92f
Marky <markdangal5858@gmail.com>
2026-08-26 13:39:38 +0545

    add properties in model named employeemodel

---
+++ HandsOnDatabase/ADO.NET/EmployeeADO/EmployeeADO/EmployeeADO/Models/EmployeeModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace EmployeeADO.Models
+{
+    public class EmployeeModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = "";
+        public string Department { get; set; } = "";
+        public decimal Salary { get; set; }
+        public string Email { get; set; } = "";
+    }
+}
