81d98364902b0996c60dbd24cffd4fd49701053c
Marky <markdangal5858@gmail.com>
2026-08-26 13:54:44 +0545

    reate a constructor that injects IConfiguration so you can pull your connection string from appsettings.json

---
+++ HandsOnDatabase/ADO.NET/EmployeeADO/EmployeeADO/EmployeeADO/Controllers/EmployeeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeADO.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public EmployeeController(IConfiguration configuration)
+        {
+            _configuration = configuration; //injection
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
