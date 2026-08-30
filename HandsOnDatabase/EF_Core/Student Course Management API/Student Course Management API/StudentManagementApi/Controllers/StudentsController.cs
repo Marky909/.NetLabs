f498d7fd039538a746c52eef82f90f4eebbe2948
Marky <markdangal5858@gmail.com>
2026-08-30 13:10:30 +0545

    Add StudentsController for CRUD operations
    Implemented StudentsController with endpoints for CRUD on Student entities using ASP.NET Core Web API. Utilized dependency injection for AppDbContext access.

---
+++ HandsOnDatabase/EF_Core/Student Course Management API/Student Course Management API/StudentManagementApi/Controllers/StudentsController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagementApi.Data;
+using StudentManagementApi.Models;
+
+namespace StudentManagementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public StudentsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudent()
+        {
+            return await _context.Students.ToListAsync();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Student>> GetStudent(int id)
+        {
+            var students = await _context.Students.FindAsync(id);
+            if (students == null)
+                return NotFound();
+            return students;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Student>> AddStudent([FromBody] Student student)
+        {
+           
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(
+                nameof(GetStudent),
+                new {Id = student.Id},
+                student);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateStudent(int id, [FromBody] Student updatedStudent)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+                return NotFound();
+
+            student.Name = updatedStudent.Name;
+            student.Email = updatedStudent.Email;
+            student.Age = updatedStudent.Age;
+            student.DepartmentId = updatedStudent.DepartmentId;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStudent(int id)
+        {
+            var student = await _context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _context.Students.Remove(student);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+
+        }
+    }
