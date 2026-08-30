a436a9e200069a502f4e174d7dbb965878e248ba
Marky <markdangal5858@gmail.com>
2026-08-30 12:36:51 +0545

    Add DepartmentController with CRUD endpoints
    Implemented DepartmentController for CRUD operations on Department entity using ASP.NET Core Web API and Entity Framework Core. Includes endpoints for retrieving all departments, getting by ID, creating, updating, and deleting departments.

---
+++ HandsOnDatabase/EF_Core/Student Course Management API/Student Course Management API/StudentManagementApi/Controllers/DepartmentController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentManagementApi.Data;
+using StudentManagementApi.Models;
+
+namespace StudentManagementApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public DepartmentController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Department>>> GetDepartment()
+        {
+            return await _context.Departments.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Department>> GetDepartment(int id)
+        {
+            var department =  await _context.Departments.FindAsync(id);
+            if (department == null)
+                return NotFound();
+            return department;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Department>> AddDepartment(Department department)
+        {
+            _context.Departments.Add(department);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(GetDepartment),
+                new { id = department.Id },
+                department
+            );
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Updatedepartment(int id, [FromBody] Department UpdatedDepartment)
+        {
+            var department = await _context.Departments.FindAsync(id);
+            if (department == null)
+                return NotFound();
+            department.Name = UpdatedDepartment.Name;
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+            
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDepartment(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+            if (department == null)
+                return NotFound();
+            _context.Departments.Remove(department);
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+    }
+}
