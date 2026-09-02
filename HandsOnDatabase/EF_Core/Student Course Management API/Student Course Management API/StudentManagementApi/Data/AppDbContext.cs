261d503d3f5aa75b1cfac9119d540d3a01675688
Marky <markdangal5858@gmail.com>
2026-09-02 16:26:42 +0545

    perform Data seeding
    data seeding was done so to add teh new data in the database when it is created

--- HandsOnDatabase/EF_Core/Student Course Management API/Student Course Management API/StudentManagementApi/Data/AppDbContext.cs
+++ HandsOnDatabase/EF_Core/Student Course Management API/Student Course Management API/StudentManagementApi/Data/AppDbContext.cs
@@ -23,6 +23,16 @@ namespace StudentManagementApi.Data
                 .Property(s => s.Name) //this is fluent api i am targetting only the name property validation
                 .IsRequired()
                 .HasMaxLength(100);
+
+            modelBuilder.Entity<Department>().HasData(
+                new Department {Id=1,Name="Computer Science", },
+                new Department { Id = 2, Name = "Information Technology"}
+                );
+
+            modelBuilder.Entity<Course>().HasData(
+                new Course { Id = 1,Name = "Database Managemnet System",Credits=3},
+                new Course { Id = 2,Name = "Web Technology",Credits=3}
+                );
         }
     }
 }
