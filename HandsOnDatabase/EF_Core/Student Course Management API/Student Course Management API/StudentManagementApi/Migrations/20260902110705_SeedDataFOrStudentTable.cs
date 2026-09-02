b10a75bd787c71451878e6bc06afd3f399a6d425
Marky <markdangal5858@gmail.com>
2026-09-02 17:10:36 +0545

    Seed Students table with Mark and Shark records
    Added EF Core migration to insert two students (Mark, Shark) into Students table. Updated model snapshot and designer files to reflect seeded data. Down method removes these records.

---
+++ HandsOnDatabase/EF_Core/Student Course Management API/Student Course Management API/StudentManagementApi/Migrations/20260902110705_SeedDataFOrStudentTable.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace StudentManagementApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDataFOrStudentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "Age", "DepartmentId", "Email", "Name" },
+                values: new object[,]
+                {
+                    { 3, 21, 1, "MarkDangal@gmail.com", "Mark" },
+                    { 4, 21, 2, "SharkAquatic@gmail.com", "Shark" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Students",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
