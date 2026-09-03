1ce4d97f9ec863a8f68f8a97618a3090678b3d67
Marky <markdangal5858@gmail.com>
2026-09-03 16:37:27 +0545

    Add UserDbContext with Users DbSet and unique UserName index
    Introduced UserDbContext in JwtAuthDotNet10.Data, inheriting from DbContext with DI for DbContextOptions. Added Users DbSet and enforced unique index on User.UserName in OnModelCreating. Included old commented DI code.

---
+++ Authentication_practice/JwtAuthDotNet10/Data/UserDbContext.cs
@@ -0,0 +1,29 @@
+﻿using JwtAuthDotNet10.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace JwtAuthDotNet10.Data
+{
+    //public class UserDbContext:DbContext
+    //{
+    //public UserDbContext(DbContextOptions<UserDbContext> options):base(options)
+    //{ boring old method of DI
+
+    //}
+    public class UserDbContext(DbContextOptions<UserDbContext> options) : DbContext(options) {
+
+        public DbSet<User> Users => Set<User>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.UserName)
+                .IsUnique();
+        }
+
+    }
+
+    
+
+}
+
