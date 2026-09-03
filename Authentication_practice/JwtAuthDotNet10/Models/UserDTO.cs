bd1777e50801330c092123b316fff7e2cf5c9fa6
Marky <markdangal5858@gmail.com>
2026-09-03 16:38:02 +0545

    Add UserDTO class with UserName and Password properties
    Created UserDTO.cs in JwtAuthDotNet10.Models namespace. The class includes two required string properties: UserName and Password.

---
+++ Authentication_practice/JwtAuthDotNet10/Models/UserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace JwtAuthDotNet10.Models
+{
+    public class UserDTO
+    {
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+    }
+}
