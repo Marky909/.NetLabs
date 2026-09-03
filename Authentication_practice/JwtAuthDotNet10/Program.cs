c6a2c9468c6e785805617e70df54338cbac22cf9
Marky <markdangal5858@gmail.com>
2026-09-03 16:39:41 +0545

    Add EF Core, Swagger, and UserDbContext registration
    Added using directives for JwtAuthDotNet10.Data and Microsoft.EntityFrameworkCore. Registered UserDbContext with DI using SQL Server and "DefaultConnection". Replaced AddOpenApi with AddEndpointsApiExplorer and AddSwaggerGen for API documentation.

--- Authentication_practice/JwtAuthDotNet10/Program.cs
+++ Authentication_practice/JwtAuthDotNet10/Program.cs
@@ -1,11 +1,15 @@
+using JwtAuthDotNet10.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<UserDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
