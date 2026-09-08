b227ce2adb0fe0cb76b2986d712c4c9b0926577f
Marky <markdangal5858@gmail.com>
2026-09-08 20:42:17 +0545

    Add core model classes for Pokémon review app
    Introduced Category, Country, Owner, Pokemon, PokemonCategory, PokemonOwner, Review, and Reviewer models in the PokemenReviewApp.Models namespace. These classes define properties and relationships to support categorization, ownership, country association, and review functionality, including navigation properties for entity relationships.

---
+++ RESTFullAPIpractice/PokemenReviewApp/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace PokemenReviewApp.Models
+{
+    public class Category
+    {
+        public  int  Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<PokemonCategory> pokemonCategorys { get; set; }
+
+
+    }
+}
---
+++ RESTFullAPIpractice/PokemenReviewApp/Models/PokemonCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace PokemenReviewApp.Models
+{
+    public class PokemonCategory
+    {
+       
+
+        public int PokemonId { get ; set ; }
+        public int CatgeoryId { get ; set ; }
+        public Pokemon Pokemon { get; set; }
+        public Category Category { get; set; }
+    }
+}
