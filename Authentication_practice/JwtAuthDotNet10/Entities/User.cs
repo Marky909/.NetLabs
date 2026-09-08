19b144c75c10f6dbe4b321f31d160c2fbc1bf054
Marky <markdangal5858@gmail.com>
2026-09-08 13:48:22 +0545

    Add Role property to User class with default value
    Added a Role property to the User class, defaulting to "User", to specify the user's role.

--- Authentication_practice/JwtAuthDotNet10/Entities/User.cs
+++ Authentication_practice/JwtAuthDotNet10/Entities/User.cs
@@ -5,5 +5,7 @@
         public Guid Id { get; set; } = Guid.NewGuid();
         public string UserName { get; set; } = "";
         public string PasswordHash { get; set; } = "";
+
+        public string Role { get; set; } = "User"; //default value
     }
 }
