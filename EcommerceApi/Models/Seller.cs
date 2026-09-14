ec4d2813193e69f8c707af2cb465614a0be76d67
Marky <markdangal5858@gmail.com>
2026-09-14 08:41:22 +0545

    Add Category, Product, Seller, and User models
    Added four new model classes under EcommerceApi.Models: Category, Product, Seller, and User. Defined properties and navigation collections to represent e-commerce entities and their relationships. These models support entity relationships for categories, products, sellers, and users.

---
+++ EcommerceApi/Models/Seller.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace EcommerceApi.Models
+{
+    public class Seller
+    {
+        public  int  Id { get; set; }
+        public  string StoreName{ get; set; }
+
+        public int UserId { get; set; }
+        public User  User { get; set; } 
+
+        public ICollection<Product> Products { get; set; }
+
+    }
+}
