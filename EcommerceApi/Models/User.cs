380a211a0a91618cd10ea80881121142d9be1471
Marky <markdangal5858@gmail.com>
2026-09-14 14:07:51 +0545

    add the passwordhash and the roles property

--- EcommerceApi/Models/User.cs
+++ EcommerceApi/Models/User.cs
@@ -7,4 +7,7 @@ public class User
     public string Username { get; set; } = string.Empty;
 
     public string Email { get; set; } = string.Empty;
+
+    public string PasswordHash { get; set; } = "";
+    public string Role { get; set; } = "User";
 }
