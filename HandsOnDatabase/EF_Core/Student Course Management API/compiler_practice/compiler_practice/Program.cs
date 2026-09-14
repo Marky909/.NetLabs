29f513a469877981dcbc7a0aeed95d493736759e
Marky <markdangal5858@gmail.com>
2026-09-14 06:59:57 +0545

    Initial project setup: Ecommerce API & compiler_practice
    Set up .NET 10.0 solutions for Ecommerce (ASP.NET Core Web API) and compiler_practice (console app). Added solution and project files. Configured OpenAPI/Swagger, controllers, launch settings, and logging for EcommerceApi. Implemented minimal Program.cs and sample HTTP request. Added console app for regex-based input validation in compiler_practice with nullable and implicit usings enabled.

---
+++ HandsOnDatabase/EF_Core/Student Course Management API/compiler_practice/compiler_practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the teststring: ");
+        string teststring = Convert.ToString(Console.ReadLine()) ?? "";
+        Console.Write("Enter the testpattern: ");
+        string testIdentifier = Convert.ToString(Console.ReadLine()) ?? "";
+        //string[] testStrings = { "", "a", "aa", "b", "ab", "aab", "abb", "aabb" };
+        string pattern = @"^(a*|a*b+|abb)$";
+        string identifierPattern = @"^[A-Za-z_][A-Za-z0-9_]*$";
+
+        if (Regex.IsMatch(teststring, pattern))
+            Console.WriteLine($"{teststring} → Accepted");
+        else                     
+            Console.WriteLine($"{teststring} → Rejected");
+
+        if (Regex.IsMatch(testIdentifier, identifierPattern))
+            Console.WriteLine($"{testIdentifier} → Accepted");
+        else
+            Console.WriteLine($"{testIdentifier} → Rejected");
+
+    }
+}
+
+
