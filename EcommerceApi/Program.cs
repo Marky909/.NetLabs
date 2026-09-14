4bf4fb7d8a64e3182b48072a4dccaf46be1b7c1c
Marky <markdangal5858@gmail.com>
2026-09-14 08:43:56 +0545

    Configure DbContext with SQL Server in DI container
    Added using directives for EcommerceApi.Data and EF Core.
    Registered EcommerceDbContext in DI with AddDbContext,
    configured to use SQL Server and "DefaultConnection".

--- EcommerceApi/Program.cs
+++ EcommerceApi/Program.cs
@@ -1,7 +1,10 @@
+using EcommerceApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<EcommerceDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -9,7 +12,7 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment()) 
 {
     app.MapOpenApi();
 }
