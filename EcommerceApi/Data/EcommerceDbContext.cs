6125d1d3bae9270b7e2ebceb5af7ac9792d6e914
Marky <markdangal5858@gmail.com>
2026-09-14 08:42:51 +0545

    add dbContext class so to access the database using ef core

---
+++ EcommerceApi/Data/EcommerceDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EcommerceApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApi.Data
+{
+    public class EcommerceDbContext(DbContextOptions<EcommerceDbContext> options) : DbContext(options)
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> products { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+    }
+}
