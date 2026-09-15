d82a3cc68e337fdafe2122d0b7b446a57a187055
Marky <markdangal5858@gmail.com>
2026-09-15 12:28:39 +0545

    Add AuthController with DI for DbContext and PasswordHasher
    Introduced AuthController as an API controller in AuthController.cs. Configured dependency injection for EcommerceDbContext and IPasswordHasher<User>. Added necessary using directives for dependencies and models. Prepares groundwork for authentication features.

---
+++ EcommerceApi/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using EcommerceApi.Data;
+using EcommerceApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly EcommerceDbContext _context;
+        private readonly IPasswordHasher<User> _passwordHasher;
+
+
+        public AuthController(EcommerceDbContext context,IPasswordHasher<User> passwordHasher)
+        {
+            _context = context;
+            _passwordHasher = passwordHasher;
+        }
+    }
+}
