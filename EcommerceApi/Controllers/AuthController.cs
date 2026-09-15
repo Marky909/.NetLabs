8f4fa7e56fcf2235c19045c16d6ab50ecaabc7da
Marky <markdangal5858@gmail.com>
2026-09-15 12:52:28 +0545

    Add Register endpoint to AuthController
    Implemented user registration with email check, password hashing, and user creation. Returns success message with user details.

--- EcommerceApi/Controllers/AuthController.cs
+++ EcommerceApi/Controllers/AuthController.cs
@@ -1,8 +1,10 @@
 ﻿using EcommerceApi.Data;
+using EcommerceApi.DTOs;
 using EcommerceApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceApi.Controllers
 {
@@ -18,6 +20,34 @@ namespace EcommerceApi.Controllers
         {
             _context = context;
             _passwordHasher = passwordHasher;
+        }
+
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterRequest request)
+        {
+            var existingUser = await _context.Users.AnyAsync(u => u.Email == request.Email);
+            if (existingUser)
+                return BadRequest("Email is already Registered");
+            var user = new User
+            {
+                Username=request.Username,
+                Email=request.Email
+            };
+
+            user.PasswordHash = _passwordHasher.HashPassword(user, request.Password);
+
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok( new
+               { message = "User registered successfully",
+                user.Id,
+                user.Username,
+                user.Email,
+                user.Role }
+                );
+
         }
     }
 }
