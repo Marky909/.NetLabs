4e8729332cfcb2a5eed591c18a47c8e40b71dc8d
Marky <markdangal5858@gmail.com>
2026-09-15 14:29:55 +0545

    Improve JWT key retrieval with fail-fast error handling
    Updated code to throw InvalidOperationException if JWT key is missing in configuration, ensuring clearer errors and preventing null assignments.

--- EcommerceApi/Program.cs
+++ EcommerceApi/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-var jwtKey = builder.Configuration["Jwt:key"]
+string jwtKey = builder.Configuration["Jwt:key"]
     ?? throw new InvalidOperationException("Jwt key is missing");
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
