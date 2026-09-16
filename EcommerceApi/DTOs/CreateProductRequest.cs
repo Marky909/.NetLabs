3fb923d799a5ded35249d64507fad49582d1271a
Marky <markdangal5858@gmail.com>
2026-09-16 19:25:33 +0545

    Add validation attributes to product request models
    Data annotation attributes ([Required], [StringLength], [Range]) were added to CreateProductRequest and UpdateProductRequest properties to enforce validation on Name, Price, Stock, and CategoryId. Imported System.ComponentModel.DataAnnotations to support these changes.

--- EcommerceApi/DTOs/CreateProductRequest.cs
+++ EcommerceApi/DTOs/CreateProductRequest.cs
@@ -1,13 +1,18 @@
-﻿namespace EcommerceApi.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApi.DTOs;
 
 public class CreateProductRequest
 {
+    [Required]
+    [StringLength(200,MinimumLength =2)]
     public string Name { get; set; } = string.Empty;
-
+    [Range(0.01,double.MaxValue)]
     public decimal Price { get; set; }
-
+    [Range(0,int.MaxValue)]
     public int Stock { get; set; }
 
+    [Range(1, int.MaxValue)]
 
     public int CategoryId { get; set; }
 }
--- EcommerceApi/DTOs/UpdateProductRequest.cs
+++ EcommerceApi/DTOs/UpdateProductRequest.cs
@@ -1,9 +1,19 @@
-﻿namespace EcommerceApi.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApi.DTOs;
 
 public class UpdateProductRequest
 {
+    [Required]
+    [StringLength(200, MinimumLength = 2)]
     public string Name { get; set; } = string.Empty;
+    [Range(0.01, int.MaxValue)]
+
     public decimal Price { get; set; }
+    [Range(0, int.MaxValue)]
+
     public int Stock { get; set; }
+    [Range(1, int.MaxValue)]
+
     public int CategoryId { get; set; }
 }
