6d7a2298b1c7dc16a73e7871c20a142ae5e8281d
Marky <markdangal5858@gmail.com>
2026-09-16 20:13:12 +0545

    Add SellerController with GET api/seller endpoint
    Added SellerController using dependency injection for EcommerceDbContext. Exposes GET api/seller to retrieve all sellers as SellerResponse DTOs (Id, StoreName) with no tracking, returning HTTP 200 OK.

---
+++ EcommerceApi/Controllers/SellerController.cs
@@ -0,0 +1,29 @@
+﻿using EcommerceApi.Data;
+using EcommerceApi.DTOs;
+using EcommerceApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EcommerceApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SellerController(EcommerceDbContext _context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Seller> GetSeller()
+        {
+            var sellers = _context.Sellers
+                .AsNoTracking()
+                .Select(s => new SellerResponse
+                {
+                    Id = s.Id,
+                    StoreName = s.StoreName
+                })
+                .ToListAsync();
+
+            return Ok(sellers);
+        }
+    }
+}
