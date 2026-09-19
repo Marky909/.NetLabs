4d11583132b1d6a9e96c5ce50034fc313ed07aa8
Marky <markdangal5858@gmail.com>
2026-09-19 18:01:50 +0545

    Suppress null warnings for User; require Items init
    The User property uses the null-forgiving operator to suppress nullability warnings. The Items property is now required and must be set during object initialization.

--- EcommerceApi/Models/Cart.cs
+++ EcommerceApi/Models/Cart.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public int UserId { get; set; }
-        public User User { get; set; }
-        public IEnumerable<CartItem> Items { get; set; } 
+        public User User { get; set; } = null!;
+        public required IEnumerable<CartItem> Items { get; set; }
     }
 }
