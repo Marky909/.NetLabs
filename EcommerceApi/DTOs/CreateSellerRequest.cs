546030ad32e640bb5a772468eb2c6f4de2a0ed4e
Marky <markdangal5858@gmail.com>
2026-09-19 13:32:51 +0545

    remove the userid so that user cant assign the user as seller gonna use jwt for that

--- EcommerceApi/DTOs/CreateSellerRequest.cs
+++ EcommerceApi/DTOs/CreateSellerRequest.cs
@@ -1,9 +1,12 @@
-﻿namespace EcommerceApi.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApi.DTOs
 {
     public class CreateSellerRequest
     {
-        public int UserId { get; set; }
-        public string StoreName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string StoreName { get; set; } = string.Empty;
 
     }
 }
