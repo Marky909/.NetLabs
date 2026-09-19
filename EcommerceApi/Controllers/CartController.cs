b9fffb965a230f9d3bdda1c25d6d35af5cdeecc6
Marky <markdangal5858@gmail.com>
2026-09-19 18:08:03 +0545

    Add CartController and AddToCartRequest DTO
    Added CartController as an API controller with authorization and routing, using EcommerceDbContext. Introduced AddToCartRequest DTO with validated ProductId and QuantityId properties.

---
+++ EcommerceApi/Controllers/CartController.cs
@@ -0,0 +1,16 @@
+﻿using EcommerceApi.Data;
+using EcommerceApi.DTOs;
+using EcommerceApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace EcommerceApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[Authorize]
+public class CartController(EcommerceDbContext context) : ControllerBase
+{
+}
---
+++ EcommerceApi/DTOs/AddToCartRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApi.DTOs
+{
+    public class AddToCartRequest
+    {
+        [Range(1, int.MaxValue)]
+        public int ProductId { get; set; }
+        [Range(1, int.MaxValue)]
+
+        public int QuantityId  { get; set; }
+    }
+}
