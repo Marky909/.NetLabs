bc92aead7f63f797b2ae22a8f08c452ba6fc195b
Marky <markdangal5858@gmail.com>
2026-09-19 20:19:22 +0545

    Rename QuantityId to Quantity in AddToCartRequest
    Renamed the property QuantityId to Quantity in the AddToCartRequest class to accurately reflect its purpose. The data annotation and type remain unchanged.

--- EcommerceApi/DTOs/AddToCartRequest.cs
+++ EcommerceApi/DTOs/AddToCartRequest.cs
@@ -8,6 +8,6 @@ namespace EcommerceApi.DTOs
         public int ProductId { get; set; }
 
         [Range(1, int.MaxValue)]
-        public int QuantityId  { get; set; }
+        public int Quantity  { get; set; }
     }
 }
