cfd44b32e7c218ccdae73b351ed5e9a9d78d5517
Marky <markdangal5858@gmail.com>
2026-09-19 14:02:11 +0545

    Add DbSet for Cart and CartItem to context
    Added DbSet<Cart> and DbSet<CartItem> to EcommerceDbContext to enable EF Core management of Cart and CartItem entities.

--- EcommerceApi/Data/EcommerceDbContext.cs
+++ EcommerceApi/Data/EcommerceDbContext.cs
@@ -9,6 +9,8 @@ namespace EcommerceApi.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Seller> Sellers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
