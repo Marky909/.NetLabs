20d9d1f37ae0ef904f2dcf44272e20d438adf15a
Marky <markdangal5858@gmail.com>
2026-09-20 13:00:11 +0545

    Configure OrderItem entity with EF Core settings
    Added OrderItemConfiguration class implementing IEntityTypeConfiguration<OrderItem>. Configured UnitPrice precision, required Quantity, and set up relationships and delete behaviors for Order and Product navigation properties.

---
+++ EcommerceApi/Configurations/OrderItem.cs
@@ -0,0 +1,27 @@
+﻿using EcommerceApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EcommerceApi.Configurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        builder.Property(oi => oi.UnitPrice)
+            .HasPrecision(18, 2);
+
+        builder.Property(oi => oi.Quantity)
+            .IsRequired();
+
+        builder.HasOne(oi => oi.Order)
+            .WithMany(o => o.Items)
+            .HasForeignKey(oi => oi.OrderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(oi => oi.Product)
+            .WithMany()
+            .HasForeignKey(oi => oi.ProductId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
