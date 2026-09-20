676d5f94c37f97ff6107c09ccafc63618a689519
Marky <markdangal5858@gmail.com>
2026-09-20 12:58:14 +0545

    Configure Order entity with new OrderConfiguration class
    Added OrderConfiguration implementing IEntityTypeConfiguration<Order> to enforce required properties, max length for Status, required OrderDate, and restricted delete behavior for Order-User relationship. Added necessary using directives.

---
+++ EcommerceApi/Configurations/OrderConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using EcommerceApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EcommerceApi.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.Property(o => o.Status)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(o => o.OrderDate)
+            .IsRequired();
+
+        builder.HasOne(o => o.User)
+            .WithMany()
+            .HasForeignKey(o => o.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
