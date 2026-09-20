1806d695650e2fdea0052385936b17b816e9e37d
Marky <markdangal5858@gmail.com>
2026-09-20 12:46:16 +0545

    Add UpdateCartItemRequest DTO with quantity validation
    Added UpdateCartItemRequest.cs in EcommerceApi.DTOs. The DTO includes a Quantity property with [Range(1, int.MaxValue)] to enforce a minimum value of 1.

---
+++ EcommerceApi/DTOs/UpdateCartItemRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApi.DTOs;
+
+public class UpdateCartItemRequest
+{
+    [Range(1, int.MaxValue)]
+    public int Quantity { get; set; }
+}
