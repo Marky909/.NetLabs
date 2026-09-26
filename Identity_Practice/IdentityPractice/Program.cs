e34f3d659ff62065d9516f25267d7cc0f2964c2b
Marky <markdangal5858@gmail.com>
2026-09-26 16:09:36 +0545

    Rework launchSettings with profiles and iisSettings
    Introduce top-level "profiles" (http, https, IIS Express) and move dotnetRunMessages, launchBrowser, applicationUrl, and environmentVariables into their respective profiles with adjusted ordering. Relocate "$schema" below profiles. Add new "iisSettings" (windowsAuthentication, anonymousAuthentication) and move IIS Express applicationUrl/sslPort into iisSettings.iisExpress; give "IIS Express" an explicit commandName. Update the https profile to expose both HTTPS and HTTP URLs.

--- Identity_Practice/IdentityPractice/Program.cs
+++ Identity_Practice/IdentityPractice/Program.cs
@@ -1,21 +1,57 @@
+using IdentityPractice.Data;
+using IdentityPractice.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add MVC
 builder.Services.AddControllersWithViews();
+
+// Database
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Identity
+builder.Services
+    .AddIdentityCore<ApplicationUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddSignInManager();
+
+// Password settings
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequireDigit = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+});
+
+// Authentication cookie
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.LoginPath = "/Account/Login";
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+    options.SlidingExpiration = true;
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure HTTP request pipeline
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+
 app.UseRouting();
 
+// Authentication MUST come before Authorization
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
@@ -24,6 +60,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 
 app.Run();
